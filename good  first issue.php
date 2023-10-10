c46510be0f52d64d6dfc3605fb4638afe1709328
ganeshtekale7700 <143712366+ganeshtekale7700@users.noreply.github.com>
2023-10-10 09:06:59 +0530

    Update good  first issue.php
    <meta name="viewport" content="width=device-width, initial-scale=1">
    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
    <meta name="keywords" content="Resort Inn Responsive , Smartphone Compatible web template , Samsung, LG, Sony Ericsson, Motorola web design" />
    <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
    		function hideURLbar(){ window.scrollTo(0,1); } </script>

--- good  first issue.php
+++ good  first issue.php
@@ -4,7 +4,7 @@ include('db.php');
 <!DOCTYPE html>
 <html lang="en">
 <head> 
-<title>PUNERI HOTEL</ title>
+<title>PUNERI HOTEL</ title> 
 <!-- for-mobile-apps -->
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />

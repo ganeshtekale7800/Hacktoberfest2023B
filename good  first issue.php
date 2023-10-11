a3f9c0926d7e5e4f2ee72f0bc48f14940dd2d736
ganeshtekale7800 <147368995+ganeshtekale7800@users.noreply.github.com>
2023-10-11 19:19:57 +0530

    Update good  first issue.php
    CREATE TABLE IF NOT EXISTS `payment` (
      `id` int(11) DEFAULT NULL,
      `title` varchar(5) DEFAULT NULL,
      `fname` varchar(30) DEFAULT NULL,
      `lname` varchar(30) DEFAULT NULL,
      `troom` varchar(30) DEFAULT NULL,
      `tbed` varchar(30) DEFAULT NULL,
      `nroom` int(11) DEFAULT NULL,
      `cin` date DEFAULT NULL,
      `cout` date DEFAULT NULL,
      `ttot` double(8,2) DEFAULT NULL,
      `fintot` double(8,2) DEFAULT NULL,
      `mepr` double(8,2) DEFAULT NULL,
      `meal` varchar(30) DEFAULT NULL,
      `btot` double(8,2) DEFAULT NULL,
      `noofdays` int(11) DEFAULT NULL
    ) ENGINE=InnoDB DEFAULT CHARSET=latin1;

--- good  first issue.php
+++ good  first issue.php
@@ -5,6 +5,10 @@ include('db.php');
 <html lang="en"> 
 <head>  
 <title>PUNERI HOTEL</ title> 
+
+
+
+	
 <!-- for-mobile-apps -->
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />

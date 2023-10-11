122150764df7c9f6278852d2e93b94e7dfae7917
ganeshtekale7700 <143712366+ganeshtekale7700@users.noreply.github.com>
2023-10-11 19:00:18 +0530

    Update good  first issue.php
    <link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
    <link href="css/font-awesome.css" rel="stylesheet"> 
    <link rel="stylesheet" href="css/chocolat.css" type="text/css" media="screen">
    <link href="css/easy-responsive-tabs.css" rel='stylesheet' type='text/css'/>
    <link rel="stylesheet" href="css/flexslider.css" type="text/css" media="screen" property="" /56444443134333>
    <link rel="stylesheet" href="css/jquery-ui.css" />

--- 01/good  first issue.php
+++ 01/good  first issue.php
@@ -6,7 +6,7 @@ include('db.php');
 <head>
 <title>PUNERI HOTEL</title>
 <!-- for-mobile-apps --> 
-<meta name="viewport" content="width=device-width, initial-scale=1"      /45464655554>
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="keywords" content="Resort Inn Responsive , Smartphone Compatible web template , Samsung, LG, Sony Ericsson, Motorola web design" />
 <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
@@ -16,7 +16,7 @@ include('db.php');
 <link href="css/font-awesome.css" rel="stylesheet"> 
 <link rel="stylesheet" href="css/chocolat.css" type="text/css" media="screen">
 <link href="css/easy-responsive-tabs.css" rel='stylesheet' type='text/css'/>
-<link rel="stylesheet" href="css/flexslider.css" type="text/css" media="screen" property="" /56444443134333>
+<link rel="stylesheet" href="css/flexslider.css" type="text/css" media="screen" property="">
 <link rel="stylesheet" href="css/jquery-ui.css" />
 <link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
 <script type="text/javascript" src="js/modernizr-2.6.2.min.js"></script>
